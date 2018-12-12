8bb28e02eabe960a2180f2f491ccd02292b52c5c
Akash <akash.patel@manektech.com>
2018-12-12 18:52:22 +0530

    Laravel Contact Form

---
+++ packages/lws/contactform/src/ContactFormServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace lws\contactform;
+use Illuminate\Support\ServiceProvider;
+
+class ContactFormServiceProvider extends ServiceProvider {
+
+    public function boot()
+    {
+      $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+      $this->loadViewsFrom(__DIR__.'/resources/views', 'contactform');
+      $this->loadMigrationsFrom(__DIR__.'/Database/migrations');
+    }
+
+
+
+    public function register()
+    {
+      
+    }
+
+}
+?>
